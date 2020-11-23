17adca2f2ca4fe1d6b986dfe4b919ec94a770de9
9631kunn <infourbh+1@gmail.com>
2020-11-23 22:15:17 +0900

    add: MongoDB

--- api/app/app.js
+++ api/app/app.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser"); // HTMLのform受け取りを簡単にできるやつ（すでに標準装備されてるぽい）
+const mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/ExpressAPI");
+mongoose.connection.on("error", (error) => {
+  console.error("MongoDB connection error: " + error);
+  process.exit(-1);
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
--- api/app/models/route/v1/user.js
+++ api/app/models/route/v1/user.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const userModel = require("../../userModel");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json({
-    message: "ワイはuserやで",
+  const User = new userModel();
+
+  User.name = req.body.name;
+  User.id = req.body.id;
+  User.bio = req.body.bio;
+
+  User.save((error) => {
+    if (error) res.send(err);
+    res.json({ message: "SUCCESS!" });
   });
 });
 
