ba8b5744a52dd9035f062b48f0c820ca7db0da0b
9631kunn <infourbh+1@gmail.com>
2020-11-24 15:27:57 +0900

    add: json api

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 - [わかりやすそう](https://gist.github.com/mitsuruog/fc48397a8e80f051a145)
 - ./api => [ここ](https://qiita.com/ngmr_mo/items/73cc7160d002a4989416)
 - ./api2 => [ここ](https://qiita.com/nkjm/items/723990c518acfee6e473)
+- ./api3 => [ここ](https://sbfl.net/blog/2018/08/25/nodejs-express-webapi/)
--- api3/app.js
+++ api3/app.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("HELLO"));
+// app.get("/", (req, res) => res.send("HELLO"));
+
+app.get("/", (req, res) => {
+  const todoList = [
+    { title: "ごはん", done: true },
+    { title: "うんこ", done: true },
+    { title: "ねむる", done: false },
+  ];
+
+  res.json(todoList);
+});
 
 app.listen(3000, () => {
   console.log("STARTED");
